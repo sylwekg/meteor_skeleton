77ee109a2167090b3b5c276b49a0e9c246f10326
Sylwester Guzek <Sylwester.guzek@wp.pl>
2018-02-02 16:52:59 +0100

    client synchronized with database on the server

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
-// import { withTracker } from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
-
+import { Resolutions } from '../api/resolutions';
+import { withTracker } from 'meteor/react-meteor-data';
+import ResolutionsForm from './ResolutionsForm';
+import ResolutionSingle from './ResolutionSingle';
 // App component - represents the whole app
-export default class App extends Component {
-  constructor(props) {
-      super(props);
-    
-      this.state = {
-        hideCompleted: false,
-      };
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            hideCompleted: false,
+        };
   }
 
   render() {
+      console.log(this.props.resolutions)
+      let list = this.props.resolutions.map( (item, index) => {
+        return <ResolutionSingle resolution={item} key={index} />
+      })
+      console.log("LIST: ",list)
       return (
       <div className="container">
-        app skeleton for meteor - react
+        <h1> My Resolutions </h1>
+        <div>
+            <ResolutionsForm /> 
+            { list.length > 0 && 
+            <ul> {list} </ul> 
+            }
+        </div>
       </div>
       );
   }
 }
 
+export default withTracker(() => {
+    Meteor.subscribe('resolutions');
+  
+    return {
+        resolutions: Resolutions.find({} ).fetch(),
+
+    };
+  })(App);
---
+++ imports/ui/ResolutionsForm.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class ResolutionsForm extends Component {
+    addResolution(event) {
+        event.preventDefault();
+        let text=this.refs.resolution.value.trim();
+        Meteor.call('resolutions.insert', text);
+        this.refs.resolution.value=""
+    }
+  
+    render() {
+        return (
+            <form className="new-resolutions" onSubmit={this.addResolution.bind(this)} >
+                <input type="text" ref="resolution" placeholder="example: Finish React Meteor Series" />
+            </form>
+        );
+    }
+}
