3631d6445d980aff304e2f5756de02871000e8fe
Sylwester Guzek <Sylwester.guzek@wp.pl>
2018-02-21 18:11:17 +0100

    initial configuration for semantic-ui

--- imports/ui/MainLayout.js
+++ imports/ui/MainLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import AccountsUI from './AccountsUI'
-
+import AccountsUI from './AccountsUI';
+import { Button } from 'semantic-ui-react';
 
 export const MainLayout = ({content}) => (
     <div className="main-layout">
@@ -10,11 +10,19 @@ export const MainLayout = ({content}) => (
             <a href="/"> Resolutions </a>
             <a href="/about"> About </a>
             <AccountsUI />
-
+            <Button primary>Click Here</Button>
+            <Button animated='fade'>
+                <Button.Content visible>
+                    Sign-up for a Pro account
+                </Button.Content>
+                <Button.Content hidden>
+                    $12.99 a month
+                </Button.Content>
+            </Button>
         </nav>
     </header>
     <main>
-
+        
         {content}
     </main>
     </div>
