2e39c7624d052e178c01881cb6d720d5d8bffa31
Sylwester Guzek <Sylwester.guzek@wp.pl>
2018-02-22 14:31:56 +0100

    preliminary main layout with responsive menu

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -26,20 +26,36 @@ class App extends Component {
       console.log("LIST: ",list)
       return (
       <div className="container">
-        <h1> My Resolutions </h1>
+        <h1> Home page </h1>
         <div>
             <ResolutionsForm /> 
-            <CSSTransitionGroup
+            {/* <CSSTransitionGroup
                 transitionName="example"
                 transitionEnterTimeout={500}
                 transitionLeaveTimeout={300}
-            >
+            > */}
             { list.length > 0 && 
             <ul> {list} </ul> 
             }
-            </CSSTransitionGroup>
+            {/* </CSSTransitionGroup> */}
             <br />
             <h1> {Session.get('test')} </h1>
+
+
+            <span> Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, 
+            non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. 
+            Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. 
+            Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum 
+            dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. 
+            Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, 
+            eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis 
+            quis, varius in, purus. Integer ultrices posuere cubilia Curae, Nulla ipsum dolor lacus, suscipit 
+            adipiscing. Cum sociis natoque penatibus et ultrices volutpat. Nullam wisi ultricies a, gravida vitae, 
+            dapibus risus ante sodales lectus blandit eu, tempor diam pede cursus vitae, ultricies eu,
+            faucibus quis, porttitor eros cursus lectus, pellentesque eget, bibendum a, gravida ullamcorper quam. 
+            Nullam viverra consectetuer. Quisque cursus et, porttitor risus. Aliquam sem. In hendrerit nulla quam 
+            nunc, accumsan congue. Lorem ipsum primis in nibh vel risus. Sed vel lectus. Ut sagittis, ipsum  
+            </span>
         </div>
       </div>
       );
--- imports/ui/MainLayout.js
+++ imports/ui/MainLayout.js
@@ -1,29 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AccountsUI from './AccountsUI';
-import { Button } from 'semantic-ui-react';
+import { Container, Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import { Responsive } from 'semantic-ui-react';
 
-export const MainLayout = ({content}) => (
-    <div className="main-layout">
-    <header>
-        <h2> My Resolutions </h2>
-        <nav>
-            <a href="/"> Resolutions </a>
-            <a href="/about"> About </a>
-            <AccountsUI />
-            <Button primary>Click Here</Button>
-            <Button animated='fade'>
-                <Button.Content visible>
-                    Sign-up for a Pro account
-                </Button.Content>
-                <Button.Content hidden>
-                    $12.99 a month
-                </Button.Content>
-            </Button>
-        </nav>
-    </header>
-    <main>
-        
-        {content}
-    </main>
-    </div>
-)
+class SidebarLeftPush extends Component {
+    render() {
+      return (
+        <Responsive  maxWidth={768}>     
+          <Sidebar.Pushable as={Segment}>
+            <Sidebar as={Menu} animation='push' width='thin' visible={this.props.visible} icon='labeled' vertical inverted>
+              <Menu.Item name='home' href='/' >
+                <Icon name='home' />
+                Home
+              </Menu.Item>
+              <Menu.Item name='gamepad'>
+                <Icon name='gamepad' />
+                Games
+              </Menu.Item>
+              <Menu.Item name='camera'>
+                <Icon name='camera' />
+                Channels
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher>
+              <Segment basic>
+                {this.props.content}
+              </Segment>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+        </Responsive>
+        )
+    }
+}
+
+export class MainLayout extends Component {
+    state = { 
+        activeItem: 'home',
+        visible: false
+    };
+  
+    toggleVisibility = () => this.setState({ visible: !this.state.visible });
+
+    handleItemClick = (e, { name }) => {
+        this.setState({ activeItem: name });
+    };
+
+    render() {
+        const { activeItem } = this.state
+
+        return(
+            <div className="main-layout">
+            <header>
+                <Menu icon inverted style={{ padding: '1em 1em' }}  >
+                    <Responsive  maxWidth={768} > 
+                        <Button 
+                            style={{background:'none', padding: '0em'  }}
+                            size='small' 
+                            onClick={this.toggleVisibility}
+                            >
+                                <Icon inverted name='bars' size='large' />
+                        </Button>
+                    </Responsive> 
+                    <Menu.Header> <h2> <a href="/"> My Resolutions </a> </h2> </Menu.Header>
+                    <Responsive  minWidth={769} style={{ position: 'absolute', right: '0px' }}> 
+                        <Menu.Menu position='right' >
+                               
+                            <Menu.Item href='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+                            <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+                            <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+                            <Menu.Item href='/about' name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
+                            <Menu.Item>  <AccountsUI /> </Menu.Item>
+                                
+                        </Menu.Menu>
+                   </Responsive >
+                </Menu>
+            </header>
+            <main>
+                <Responsive  minWidth={769}>
+                <Container>
+                    {this.props.content}
+                </Container>
+                </Responsive>
+                <SidebarLeftPush content={this.props.content} visible={this.state.visible} />  
+            </main>
+            </div>  
+        );
+    }
+}
+
