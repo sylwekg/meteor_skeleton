5c8027f0cd095fefd7714599131d60adf70d8a2a
Sylwester Guzek <Sylwester.guzek@wp.pl>
2018-02-28 09:25:10 +0100

    tesing libraries and tuning for heroku deployment

--- server/main.js
+++ server/main.js
@@ -5,5 +5,5 @@ import '../imports/api/resolutions.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  console.log(Meteor.settings.private.ptest)
+  // console.log(Meteor.settings.private.ptest)
 });
